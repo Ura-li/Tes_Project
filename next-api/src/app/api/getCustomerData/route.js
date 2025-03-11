038ec1d397bd64ae47836e61720dedd584c6c5ea
Miku21 <99104875+Miku21750@users.noreply.github.com>
2025-03-11 13:48:18 +0700

    Adding Axios Library

--- next-api/src/app/api/getCustomerData/route.js
+++ next-api/src/app/api/getCustomerData/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
 import prisma from "../../../../prisma/client"
 
-export async function GET(req, res) {
+export async function GET(request) {
     //get search parameter
     const { searchParams } = new URL(request.url);
 
@@ -11,18 +11,24 @@ export async function GET(req, res) {
     const first_name = searchParams.get("first_name")
     const last_name = searchParams.get("last_name")
     const email = searchParams.get("email")
+    const company = searchParams.get("company")
 
     //prisma query filter
     const filters = {
-        SerialNumber : serialNumber ? serialNumber : undefined,
+        SerialNumber : serialNumber ? { contains: serialNumber } : undefined,
+        ProductName : productName ? { contains: productName } : undefined,
+        FirstName : first_name ? { contains: first_name } : undefined,
+        LastName :last_name ? { contains:last_name } : undefined,
+        Email : email ? { contains: email } : undefined,
+        Company : company ? { contains: company } : undefined,
     };
-    if(serialNumber){
-        filters.SerialNumber = { contains: serialNumber }
-    }
-    if(productName){
-        filters.ProductName = { contains: productName }
-    }
     //get all data
+    /**
+     * TODO
+     * INTINYA BISA, TAPI GABISA COMBINE (maybe)
+     * Solution :
+     * make an switch case where criteria is.
+     */
     const asset_information = await prisma.asset_information.findMany({
         where: Object.keys(filters).length > 0 ? filters : undefined,
         include:
@@ -34,7 +40,7 @@ export async function GET(req, res) {
     return NextResponse.json(
         {
             success: true,
-            message: "List Data Asset Information",
+            message: "List Data",
             data: asset_information,
         },
         {
