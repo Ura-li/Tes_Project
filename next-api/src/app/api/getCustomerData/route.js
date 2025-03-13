8da82ef3dd09a7fde2d857fe80a122eefff46a04
Miku21 <99104875+Miku21750@users.noreply.github.com>
2025-03-13 09:12:30 +0700

    feat: Add CORS headers, search functionality, and API integration (#8)
    * Adding Package Express for API
    
    * lol
    
    * Adding a API server side Backend for SQL
    
    * Adding SQL
    
    * lol
    
    * Merge branch 'tmp'
    trying to merge
    
    * Merge branch 'main' of https://github.com/Miku21750/Tes_Project
    the commit.
    
    * adding databases
    
    * adding search route, todo adding post request
    
    * Merge branch 'main' of https://github.com/Miku21750/Tes_Project
    the commit.
    
    * Adding next.js to try API
    
    * qwe
    
    * Merge pull request #1 from truesky123/main
    
    qwe
    
    * Merge branch 'nextjs'
    
    * Adding CRUD API in NEXT.JS
    
    * Merge branch 'Mackovlak:main' into main
    
    * adding route 50%
    
    * Merge branch 'main' of https://github.com/Miku21750/Tes_Project
    
    * Adding Axios Library
    
    * Merge branch 'main' of https://github.com/Miku21750/Tes_Project
    
    * Update README.md
    
    * Readme.md (#6)
    
    * Adding Package Express for API
    
    * lol
    
    * Adding a API server side Backend for SQL
    
    * Adding SQL
    
    * lol
    
    * adding databases
    
    * adding search route, todo adding post request
    
    * Adding next.js to try API
    
    * qwe
    
    * Adding CRUD API in NEXT.JS
    
    * adding route 50%
    
    * Adding Axios Library
    
    * Update README.md
    
    ---------
    
    Co-authored-by: truesky123 <146347256+truesky123@users.noreply.github.com>
    
    * Create CONTRIBUTING.md
    
    update main rules
    
    * small change #1
    
    * Merge branch 'main' of https://github.com/Mackovlak/Tes_Project
    
    * feat: Add CORS headers, search functionality, and API integration
    
    * Merge branch 'main' of https://github.com/Miku21750/Tes_Project
    
    * Merge branch 'Mackovlak:main' into main

---
+++ next-api/src/app/api/getCustomerData/route.js
@@ -0,0 +1,50 @@
+import { NextResponse } from "next/server"
+import prisma from "../../../../prisma/client"
+
+export async function GET(request) {
+    //get search parameter
+    const { searchParams } = new URL(request.url);
+
+    //extract query parameter
+    const serialNumber = searchParams.get("SerialNumber")
+    const productName = searchParams.get("ProductName")
+    const first_name = searchParams.get("first_name")
+    const last_name = searchParams.get("last_name")
+    const email = searchParams.get("email")
+    const company = searchParams.get("company")
+
+    //prisma query filter
+    const filters = {
+        SerialNumber : serialNumber ? { contains: serialNumber } : undefined,
+        ProductName : productName ? { contains: productName } : undefined,
+        FirstName : first_name ? { contains: first_name } : undefined,
+        LastName :last_name ? { contains:last_name } : undefined,
+        Email : email ? { contains: email } : undefined,
+        Company : company ? { contains: company } : undefined,
+    };
+    //get all data
+    /**
+     * TODO
+     * INTINYA BISA, TAPI GABISA COMBINE (maybe)
+     * Solution :
+     * make an switch case where criteria is.
+     */
+    const asset_information = await prisma.asset_information.findMany({
+        where: Object.keys(filters).length > 0 ? filters : undefined,
+        include:
+        {
+            site_account: true,
+            contact_information:true
+        }
+    });
+    return NextResponse.json(
+        {
+            success: true,
+            message: "List Data",
+            data: asset_information,
+        },
+        {
+            status:200,
+        }
+    );
+}
