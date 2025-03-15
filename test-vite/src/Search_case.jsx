5645277678c8c03773a436909207a6b30e113a8c
Mayy45 <147509166+Mayy45@users.noreply.github.com>
2025-03-15 12:10:18 +0700

    New Btn Contact and Btn Asset (#17)
    * new
    
    * Readme.md (#6)
    
    * Adding Package Express for API
    
    * lol
    
    * Adding a API server side Backend for SQL
    
    * Adding SQL
    
    * lol
    
    * adding databases
    
    * adding search route, todo adding post request
    
    * Adding next.js to try API
    
    * qwe
    
    * Adding CRUD API in NEXT.JS
    
    * adding route 50%
    
    * Adding Axios Library
    
    * Update README.md
    
    ---------
    
    Co-authored-by: truesky123 <146347256+truesky123@users.noreply.github.com>
    
    * Create CONTRIBUTING.md
    
    update main rules
    
    * small change #1
    
    * new #2
    
    * newv#2
    
    * new #3
    
    * New
    
    * TERBARU
    
    Terbaru nih
    
    * Update app-sidebar.jsx
    
    * SAVE
    
    * a
    
    * New Fix
    
    * New Modal Contact and Asset
    
    * New Btn
    
    * Revert "New Btn"
    
    This reverts commit 51215c01ba32c20bced53da91bc864bb78ffe4ce.
    
    * Reapply "New Btn"
    
    This reverts commit 4a870f5c8dfbf839d0733e05dd85ec5ba58b3aef.
    
    * Revert "New Btn"
    
    This reverts commit 51215c01ba32c20bced53da91bc864bb78ffe4ce.
    
    * New = Terbaru
    
    ---------
    
    Co-authored-by: Mackovlak <mackovlak123@gmail.com>
    Co-authored-by: Miku21 <99104875+Miku21750@users.noreply.github.com>
    Co-authored-by: truesky123 <146347256+truesky123@users.noreply.github.com>
    Co-authored-by: Mackovlak <145180858+Mackovlak@users.noreply.github.com>

--- test-vite/src/Search_case.jsx
+++ test-vite/src/Search_case.jsx
@@ -55,12 +55,11 @@ import {
   TabsTrigger,
 } from "@/components/ui/tabs"
 
-import { DialogCloseButton } from './components/assets-modal'
+import { DialogCloseButton, DialogCompanyBtn } from './components/assets-modal'
 import { SelectBar } from './components/sc-select'
 import { SelectBar1 } from './components/sc-select'
 import { SelectBar2 } from './components/sc-select'
 import { BtnModal } from './components/sc-modal'
-import { TableCompany } from './components/sc-table'
 import { TableContact } from './components/sc-table'
 import { TableAsset } from './components/sc-table'
 
@@ -83,9 +82,7 @@ const Search_case = () => {
               <TabsTrigger value="search" className="cursor-pointer">Search</TabsTrigger>
               <TabsTrigger value="ci" className="cursor-pointer">Costumer Information</TabsTrigger>
             </div>
-            {/* <Button className="ml-50 cursor-pointer "><span></span>Customer Complaint</Button>
-            <Button className="cursor-pointer"><span></span>Customer Complaint Legal</Button> */}
-            {/* <Button className="mr-1.5 cursor-pointer"><span><Plus></Plus></span>Create Case</Button> */}
+
             <BtnModal></BtnModal>
           </TabsList>
           <TabsContent value="search">
@@ -158,14 +155,11 @@ const Search_case = () => {
           <TabsList className="bg-white float-right mr-5">   
               <TabsTrigger value="Account" className="cursor-pointer"><span><Plus></Plus></span>Create New</TabsTrigger>
               <DialogCloseButton></DialogCloseButton>
+              <DialogCompanyBtn></DialogCompanyBtn>
           </TabsList>
           <div className='mb-5'>
-            <TableCompany></TableCompany>
             <TableContact></TableContact>
-            <TableAsset></TableAsset>
-            {/* <ShelterBox></ShelterBox>
-            <ShelterContact></ShelterContact>
-            <ShelterAsset></ShelterAsset> */}
+            <TableAsset></TableAsset>  
           </div>
           </TabsContent>
 
@@ -184,12 +178,14 @@ const Search_case = () => {
                   Change your password here. After saving, you'll be logged out.
                 </CardDescription>
               </CardHeader> */}
-              <CardHeader>
-                <CardTitle className="flex justify-end">
+           
                   
-                </CardTitle>
-                <CardTitle >
+                <CardHeader className="flex-row justify-between">
+              <CardTitle>
                   Basic Information
+                </CardTitle>  
+                <CardTitle>
+                  Clear All
                 </CardTitle>
               </CardHeader>
               
@@ -261,12 +257,12 @@ const Search_case = () => {
                 </CardDescription>
               </CardHeader> */}
               
-              <CardHeader>
-              <CardTitle className="flex justify-end">
-                  Clear All
+              <CardHeader className="flex-row justify-between">
+              <CardTitle>
+                  Basic Information
                 </CardTitle>  
                 <CardTitle>
-                  Basic Information
+                  Clear All
                 </CardTitle>
               </CardHeader>
 
