b6a74a034f492d80046fa574388bf4ea1a66d8a7
Ura-li <ferdiazmi4@gmail.com>
2025-03-21 09:09:18 +0700

    case_table
    penamabahan case_table

--- test-vite/src/components/app-sidebar.jsx
+++ test-vite/src/components/app-sidebar.jsx
@@ -80,6 +80,10 @@ const data = {
           title: "Contact",
           url: "/master/Contact_table",
         },
+        {
+          title: "Case_Table",
+          url: "/master/Case_table",
+        },
       ],
     },
     {
