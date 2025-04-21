78ce2a4b68fe10b371ba94e87316442ce487427e
truesky123 <146347256+truesky123@users.noreply.github.com>
2025-04-21 15:29:10 +0700

    a

--- test-vite/src/Case.jsx
+++ test-vite/src/Case.jsx
@@ -1,7 +1,3 @@
-<<<<<<< Updated upstream
-import React from 'react'
-import { ServiceCase } from './components/service-case'
-=======
 import React, { useEffect, useState } from 'react'
 import { 
   ServiceCase,
@@ -9,7 +5,6 @@ import {
  } from './components/service-case'
 import { useParams } from 'react-router'
 import ApiCustomer from './api'
->>>>>>> Stashed changes
 
 export const Case = () => {
   return (
