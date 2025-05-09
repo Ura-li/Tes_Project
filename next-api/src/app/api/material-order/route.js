d5b5f42655a17dd322efc63c57906369e468c09d
truesky123 <146347256+truesky123@users.noreply.github.com>
2025-05-09 08:45:04 +0700

    Route materialorder revisi

--- next-api/src/app/api/material-order/route.js
+++ next-api/src/app/api/material-order/route.js
@@ -1,40 +1,40 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../../prisma/client";
 
-export async function GET(request) {
-    try{
-        const { searchParams } = new URL(request.url);
-        const search = searchParams.get("search") || "";
-        const woidParam = searchParams.get("WOID"); // "wo1,wo2"
+// export async function GET(request) {
+//     try{
+//         const { searchParams } = new URL(request.url);
+//         const search = searchParams.get("search") || "";
+//         const woidParam = searchParams.get("WOID"); // "wo1,wo2"
         
-        const woidArray = woidParam?.split(",") || [];
+//         const woidArray = woidParam?.split(",") || [];
         
-        // const page = parseInt(searchParams.get("page")) || 1;
-        // const limit = parseInt(searchParams.get("limit")) || 10;
+//         // const page = parseInt(searchParams.get("page")) || 1;
+//         // const limit = parseInt(searchParams.get("limit")) || 10;
 
-        // console.log("Query Params:", { search, page, limit });
-         // Initialize search filters
-         const materialOrders = await prisma.materialorder.findMany({
-            where: {
-              WOID: { in: woidArray }
-            },
-          });
+//         // console.log("Query Params:", { search, page, limit });
+//          // Initialize search filters
+//          const materialOrders = await prisma.materialorder.findMany({
+//             where: {
+//               WOID: { in: woidArray }
+//             },
+//           });
 
-        return NextResponse.json({
-            success: true,
-            message: "List Data Material Order",
-            data: materialOrders
-        });
-    }catch(err){
-        console.error("🔥 ERROR in GET API:", err);
+//         return NextResponse.json({
+//             success: true,
+//             message: "List Data Material Order",
+//             data: materialOrders
+//         });
+//     }catch(err){
+//         console.error("🔥 ERROR in GET API:", err);
 
-        return NextResponse.json({
-            success: false,
-            message: "Failed to fetch data",
-            error: err.message
-        }, { status: 500 });
-    }
-}
+//         return NextResponse.json({
+//             success: false,
+//             message: "Failed to fetch data",
+//             error: err.message
+//         }, { status: 500 });
+//     }
+// }
 
 export async function GET(request) {
   try {
