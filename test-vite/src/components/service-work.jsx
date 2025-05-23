d2cdc0c4df883f1292b5c2c98d1cc859b174b3ec
Mackovlak <mackovlak123@gmail.com>
2025-05-23 09:57:40 +0700

    feat: implement DraftContext for managing draft states across components refactor: update various components to utilize draft context for state management style: update index.html title and favicon fix: adjust login redirection path to '/app'

--- test-vite/src/components/service-work.jsx
+++ test-vite/src/components/service-work.jsx
@@ -66,11 +66,12 @@ import {
   CommandSeparator,
   CommandShortcut,
 } from "@/components/ui/command";
+import { useDraft } from "./DraftContext";
 
 export const ServiceWork = () => {
   const user = getUserFromToken();
   const { woid } = useParams();
-
+  const { updateDraft } = useDraft();
   const [workOrders, setWorkOrders] = useState([]);
   const fetchWorkOrders = async () => {
     try {
@@ -164,7 +165,8 @@ export const ServiceWork = () => {
         const resWO = await ApiCustomer.get(`/api/work-order/${woid}`);
         const workOrderData = resWO.data.data;
         setWorkOrders(workOrderData);
-
+        // console.log('Value of workorder data',workOrderData)
+        updateDraft('woid',workOrderData.WOID)
         const resMO = await ApiCustomer.get(`/api/material-order?WOID=${woid}`);
         setMaterialOrders(resMO.data.data);
 
