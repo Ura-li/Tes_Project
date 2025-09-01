d5522e3676e718798ec0d49d486519937fcc5806
Mayy45 <pakrt556@gmail.com>
2025-09-01 23:15:19 +0700

    Progres 23.15
    Update Case Details CE

--- test-vite/src/pages/CaseDetailApo.jsx
+++ test-vite/src/pages/CaseDetailApo.jsx
@@ -390,9 +390,9 @@ const openPopup = () => {
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
-    }, },
+    },hidden: true },
     { icon: StepBack, label: "CSR", onClick: () => openServiceCatalog("CSR"), hidden: true },
-    { icon: StepBack, label: "Service Order", onClick: () => openServiceCatalog("serviceorder")},
+    { icon: StepBack, label: "Service Order", onClick: () => openServiceCatalog("serviceorder"), hidden: true},
     { icon: StepBack, label: "Work Order", onClick: () => openServiceCatalog("workorder"), hidden: true },
     { icon: StepBack, label: "Sales Offer", hidden:true},
     { icon: StepBack, label: "Close Case", hidden:true },
@@ -1163,6 +1163,8 @@ export const ServiceCase = ({
   const [createdOn, setCreatedOn] = useState(null);
   const [caseClosedDate, setCaseClosedDate] = useState(null);
   const [submittedToBase, setsubmittedToBase] = useState(null);
+
+  const [cards, setCards] = useState([{}]);
 
   const [roleAssign, setRoleAssign] = useState([]);
 
@@ -2062,7 +2064,7 @@ const [endDate, setEndDate] = useState(null);
                     <Input variant="invisible" placeholder="---" />
                   </CaseField>
                 </div>
-                  <CaseField label="OTC Code" lock span={3}>
+                  <CaseField label="OTC Code" lock span={3} star>
                   <SearchCommandBlock
                     options={otcCode}
                     value={entitlementStatus.OTCCode}
@@ -3041,55 +3043,6 @@ const [endDate, setEndDate] = useState(null);
                 </Table>
               </CardContent>
             </Card>
-
-                          <Card>
-                            <CardHeader className={"text-lg"}>
-                              <CardTitle>Note PartOrder</CardTitle>
-                              <hr />
-                            </CardHeader>
-                            <CardContent className={"grid grid-cols-4 gap-4"}>
-                              <CaseField label={"Vendor part no"} lock>
-                                <Input variant={"invisible"} placeholder="---" readOnly/>
-                              </CaseField>
-                              <CaseField label={"Hp part no"} lock>
-                                <Input variant={"invisible"} placeholder="---" readOnly/>
-                              </CaseField>
-                              <CaseField label={"Part Category"} lock>
-                                <SearchCommandBlock
-                                  options={["Yes", "No"]}
-                                  readOnly
-                                  placeholder="---"
-                                />
-                              </CaseField>
-                              <CaseField label={"Part backup"} lock>
-                                <SearchCommandBlock
-                                  options={["Yes", "No"]}
-                                  readOnly
-                                  placeholder="---"
-                                />
-                              </CaseField>
-                              <CaseField label={"Part status"} lock>
-                                <Input variant={"invisible"} placeholder="---" readOnly/>
-                              </CaseField>
-                              <CaseField label={"Bad CT Code"} lock>
-                                <Input variant={"invisible"} placeholder="---" readOnly/>
-                              </CaseField>
-                              <CaseField label={"New CT Code"} lock>
-                                <Input variant={"invisible"} placeholder="---" readOnly/>
-                              </CaseField>
-                              <CaseField label={"UEFI Code"} lock>
-                                <SearchCommandBlock
-                                  options={["DHU", "FID", "MPS", "PND", "PPR"]}
-                                  placeholder="---"
-                                  readOnly
-                                />
-                              </CaseField>
-                              <CaseField label={"Part Name"} lock>
-                                <Textarea className={"resize-none h-[7em]"} readOnly/>
-                              </CaseField>
-                            </CardContent>
-                          </Card>
-                        
           </TabsContent>
 
           <TabsContent value="ci_salles">
