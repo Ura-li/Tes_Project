a14da5333e6682ff9717877d2e96365f452293c6
Miku21 <99104875+Miku21750@users.noreply.github.com>
2025-09-16 14:27:51 +0700

    test

--- test-vite/src/pages/services/service-bookingApo.jsx
+++ test-vite/src/pages/services/service-bookingApo.jsx
@@ -260,7 +260,7 @@ export function ServiceBookingApo ({BookingId , woid}) {
     try {
       await ApiCustomer.patch(`/api/bookings/${bookingid}`, {
         ChangedBy: changedBy,
-        BookingStatusId: markCompleted ? 2 : bookingid,
+        BookingStatusId: markCompleted ? 2 : parseInt(bookingid),
         DoNotDisturb: doNotDisturb,
         CeScheduleChange: ceScheduleChange,
         ScheduleJeopardy: scheduleJeopardy,
