8a088b8892a564322fd5ef3428954b4274e58a69
Karenity <147509166+Mayy45@users.noreply.github.com>
2026-01-13 04:17:53 +0700

    Progres 4:17 AM
    Salah Logic di Deadline

--- test-vite/src/pages/services/ServiceWorkReimagined.jsx
+++ test-vite/src/pages/services/ServiceWorkReimagined.jsx
@@ -343,7 +343,7 @@ export const ServiceWork = () => {
     ? "wo-deadline-past"
     : "wo-deadline-not-past";
 } else {
-  status = now > targetTime ? "belum" : "lewat";
+  status = now > targetTime ? "lewat" : "belum";
 }
 
   return (
