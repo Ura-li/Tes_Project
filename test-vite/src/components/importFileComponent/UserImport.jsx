e2cdf6f2dbbbebaabc0eda7ea3e4c048d4d70f9a
Miku21 <99104875+Miku21750@users.noreply.github.com>
2026-01-13 12:23:46 +0700

    Add user import API and UI components
    Introduces a new API route for importing users via Excel files, including template download and bulk user creation with validation and password hashing. Adds corresponding React components for user import and template download to the frontend, and integrates them into the master table page.

---
+++ test-vite/src/components/importFileComponent/UserImport.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import * as XLSX from "xlsx"
+import Swal from "sweetalert2";
+import { Button } from "../ui/button";
+import { toast } from "sonner";
+import ApiCustomer from "@/api";
+import { Input } from "../ui/input";
+
+export function UserTemplateButton() {
+  const handleDownload = () => {
+    window.location.href = `${import.meta.env.VITE_API_BASE_URL}/api/import/user`;
+  };
+
+  return (
+    <Button variant={"outline"} onClick={handleDownload}>
+      Download User Template
+    </Button>
+  );
+}
+
+
+export function UserImport() {
+    const [file, setFile] = useState(null)
+
+    const handleFileUpload = (e) =>{
+        setFile(e.target.files[0]);
+    }
+
+    const handleImport = async () => {
+        if (!file){
+            toast.warning("No File Uploaded.");
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append("file",file);
+
+        try{
+            const response = await ApiCustomer.post("/api/import/user",formData);
+            const result = response.json();
+
+            if(result.success){
+                toast(`${result.data.data.message}`)
+            }else{
+                toast.warning(`${result.data.data.message}`)
+            }
+        }catch(err){
+            toast.warning("Failed to import data")
+        }
+    }
+    return (
+        <div className="flex flex-row items-center gap-2 ">
+            <Input className="ring-1 ring-gray-400 rounded-sm" type="file" accept=".xlsx, .xls" onChange={handleFileUpload} />
+            <Button variant={"outline"} onClick={handleImport}>Import User Data</Button>
+        </div>
+    );
+}
